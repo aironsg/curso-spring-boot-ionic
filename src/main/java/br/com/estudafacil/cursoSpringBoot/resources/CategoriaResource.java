4d93cab51272cc57f895536460c74fe7ac003193
aironsg <airon_gm@hotmail.com>
2021-12-21 21:12:14 -0300

    testando classe de dominio 'Categoria'

--- src/main/java/br/com/estudafacil/cursoSpringBoot/resources/CategoriaResource.java
+++ src/main/java/br/com/estudafacil/cursoSpringBoot/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package br.com.estudafacil.cursoSpringBoot.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.estudafacil.cursoSpringBoot.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Testando funcionamento inicial Rest";
+	public List<Categoria> listar() { 
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(categoria1);
+		lista.add(categoria2);
+		
+		return lista;
 	}
 	
 

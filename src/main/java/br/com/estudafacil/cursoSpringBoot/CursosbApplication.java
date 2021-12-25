166c8759875fdda49cf8e4ec23b42404752dda00
aironsg <airon_gm@hotmail.com>
2021-12-25 10:31:23 -0300

    Cliente- Tipo cliente -  Telefones - enderecos

--- src/main/java/br/com/estudafacil/cursoSpringBoot/CursosbApplication.java
+++ src/main/java/br/com/estudafacil/cursoSpringBoot/CursosbApplication.java
@@ -10,10 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.estudafacil.cursoSpringBoot.domain.Categoria;
 import br.com.estudafacil.cursoSpringBoot.domain.Cidade;
+import br.com.estudafacil.cursoSpringBoot.domain.Cliente;
+import br.com.estudafacil.cursoSpringBoot.domain.Endereco;
 import br.com.estudafacil.cursoSpringBoot.domain.Estado;
 import br.com.estudafacil.cursoSpringBoot.domain.Produto;
+import br.com.estudafacil.cursoSpringBoot.domain.enums.TipoCliente;
 import br.com.estudafacil.cursoSpringBoot.repositories.CategoriaRepository;
 import br.com.estudafacil.cursoSpringBoot.repositories.CidadeRepository;
+import br.com.estudafacil.cursoSpringBoot.repositories.ClienteRepository;
+import br.com.estudafacil.cursoSpringBoot.repositories.EnderecoRepository;
 import br.com.estudafacil.cursoSpringBoot.repositories.EstadoRepository;
 import br.com.estudafacil.cursoSpringBoot.repositories.ProdutoRepository;
 
@@ -31,6 +36,12 @@ public class CursosbApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursosbApplication.class, args);
@@ -57,18 +68,33 @@ public class CursosbApplication implements CommandLineRunner{
 		
 		Estado estado1 = new Estado("MinasGerais");
 		Estado estado2 = new Estado("São Paulo");
+		Estado estado3 = new Estado("Pernambuco");
 		
 		Cidade cidade1 = new Cidade("Uberlândia", estado1);
 		Cidade cidade2 = new Cidade("São Paulo", estado2);
 		Cidade cidade3 = new Cidade("Campinas", estado2);
+		Cidade cidade4 = new Cidade("Caruaru", estado3);
 		
 		estado1.getCidades().addAll(Arrays.asList(cidade1));
 		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
+		estado3.getCidades().addAll(Arrays.asList(cidade4));
+		
+		
+		Cliente cliente1 = new Cliente("Airon silva", "airon@teste.com", "000.222.444-55", TipoCliente.PESSOAFISICA);
+		cliente1.getTelefone().addAll(Arrays.asList("9999-8888", "5555-7777"));
+		
+		Endereco endereco1 = new Endereco("Rua lapaz", "338", "aptB", "João Mota", "55010-080", cidade4, cliente1);
+		Endereco endereco2 = new Endereco("passo largo", "002", "casa", "não sei onde", "55000-000", cidade4, cliente1);
+		
+		
+		cliente1.getEnderecos().addAll(Arrays.asList(endereco1, endereco2));
 		
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
-		estadoRepository.saveAll(Arrays.asList(estado1,estado2));
-		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
+		estadoRepository.saveAll(Arrays.asList(estado1,estado2,estado3));
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3,cidade4));
+		clienteRepository.saveAll(Arrays.asList(cliente1));
+		enderecoRepository.saveAll(Arrays.asList(endereco1, endereco2));
 		
 		
 	}
---
+++ src/main/java/br/com/estudafacil/cursoSpringBoot/domain/Cliente.java
@@ -0,0 +1,115 @@
+package br.com.estudafacil.cursoSpringBoot.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import br.com.estudafacil.cursoSpringBoot.domain.enums.TipoCliente;
+@Entity
+public class Cliente implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String email;
+	private String cpfOrCnpj;
+	private Integer tipo;
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Endereco> enderecos = new ArrayList<>();
+	
+	@ElementCollection
+	@CollectionTable(name = "TELEFONE")
+	private Set<String> telefones = new HashSet<>();
+	
+	
+	public Cliente() {
+		
+	}
+	public Cliente(String nome, String email, String cpfOrCnpj, TipoCliente tipo) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.cpfOrCnpj = cpfOrCnpj;
+		this.tipo = tipo.getCod();
+		
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCpfOrCnpj() {
+		return cpfOrCnpj;
+	}
+	public void setCpfOrCnpj(String cpfOrCnpj) {
+		this.cpfOrCnpj = cpfOrCnpj;
+	}
+	public TipoCliente getTipo() {
+		return TipoCliente.toEnum(tipo);
+	}
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCod();
+	}
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+	public Set<String> getTelefone() {
+		return telefones;
+	}
+	public void setTelefone(Set<String> telefone) {
+		this.telefones = telefone;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+
+}

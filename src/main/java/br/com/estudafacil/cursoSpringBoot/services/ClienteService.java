34d687127c75927a6a0c5ac0ce96441caa1dfd76
aironsg <airon_gm@hotmail.com>
2021-12-26 08:28:27 -0300

    Endpoint / clientes/{id} disponivel

---
+++ src/main/java/br/com/estudafacil/cursoSpringBoot/services/ClienteService.java
@@ -0,0 +1,25 @@
+package br.com.estudafacil.cursoSpringBoot.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.estudafacil.cursoSpringBoot.domain.Cliente;
+import br.com.estudafacil.cursoSpringBoot.repositories.ClienteRepository;
+import br.com.estudafacil.cursoSpringBoot.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscarCliente(Integer id) {
+		Optional<Cliente> objCliente = repo.findById(id);
+		return objCliente.orElseThrow(() -> new ObjectNotFoundException("O objeto não foi encontrado" +
+		id + ", Tipo : " + Cliente.class.getName()));
+		
+	}
+
+}

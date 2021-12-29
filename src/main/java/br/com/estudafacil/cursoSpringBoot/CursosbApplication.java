0cab1ee599df521590652324cf97a7abe9b3fdc5
aironsg <airon_gm@hotmail.com>
2021-12-29 19:14:23 -0300

    Inserindo uma nova Categoria com o metodo POST

--- src/main/java/br/com/estudafacil/cursoSpringBoot/CursosbApplication.java
+++ src/main/java/br/com/estudafacil/cursoSpringBoot/CursosbApplication.java
@@ -145,6 +145,8 @@ public class CursosbApplication implements CommandLineRunner{
 		
 		
 		
+		
+		
 	}
 
 }
--- src/main/java/br/com/estudafacil/cursoSpringBoot/domain/Categoria.java
+++ src/main/java/br/com/estudafacil/cursoSpringBoot/domain/Categoria.java
@@ -35,6 +35,12 @@ public class Categoria implements Serializable{
 	public Integer getId() {
 		return id;
 	}
+	
+	
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
